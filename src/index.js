71c78c5d6d19b50385a8d179400f399937bf8ddc
smishra11 <msubhasish11@gmail.com>
2021-01-26 00:20:39 +0530

    changes for showing movie catagories

--- src/index.js
+++ src/index.js
@@ -5,20 +5,26 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './store/reducer/reducer';
-import logger from 'redux-logger';
+// import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
+// import reducer from './store/reducer/reducer';
+// import logger from 'redux-logger';
+// import thunkMiddleware from 'redux-thunk';
 
-const store = createStore(reducer, applyMiddleware(logger));
+// const store = createStore(reducer, applyMiddleware(logger, thunkMiddleware));
 
-ReactDOM.render(
+// const app = (
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>
+// );
+const app = (
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
+ReactDOM.render(app, document.getElementById('root'));
 
 reportWebVitals();
