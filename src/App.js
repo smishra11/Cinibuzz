3514848fb3eddb4399aaa70a70c8e276130e8868
smishra11 <msubhasish11@gmail.com>
2021-01-27 20:41:01 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import MovieDetails from './components/Movie/MovieDetails';
 import Homepage from './components/Homepage/Homepage';
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        {/* <Homepage /> */}
+        <Route path="/movie_id:slug" component={MovieDetails} />
+      </Switch>
+    </Router>
   );
 }
 
